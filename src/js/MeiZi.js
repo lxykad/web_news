25ae39a10e437e8e6023166b816b77b5fff61b4a
lxykad <503215875@qq.com>
2017-12-26 22:34:37 +0800

    add meizi page image

--- src/js/MeiZi.js
+++ src/js/MeiZi.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import TimeUtil from '../utils/TimeUtil'
-import List from 'antd/lib/list';
-import 'antd/lib/list/style/css'
-import 'antd/lib/avatar/style/css'
+import '../css/pc.css'
 
-class MeiZi extends Component{
+import Masonry from 'react-masonry-component';
+var masonryOptions = {
+    transitionDuration: 0
+};
+
+class MeiZi extends Component {
 
     constructor() {
         super();
         this.state = {
             name: 'default',
             url: 'http://gank.io/api/data/福利/20/1',
-            list: []
+            list: [],
+            height:this.randomNum(200,400)
         }
 
+    }
+
+    randomNum(minNum,maxNum){
+        switch(arguments.length){
+            case 1:
+                return parseInt(Math.random()*minNum+1,10);
+                break;
+            case 2:
+                return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10);
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+
+    onImgClick(item){
+        console.log(item.createdAt)
     }
 
     componentWillMount() {
@@ -21,9 +43,29 @@ class MeiZi extends Component{
         this.loadData()
     }
 
-    render(){
-        return(
-            <div>mei</div>
+    render() {
+        return (
+
+            <Masonry
+                className="image-element-class"
+                elementType={'ul'}
+                options={masonryOptions} // default {}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+            >
+
+                {
+                    this.state.list.map(item => {
+                        return (
+
+                            <li className="image-element-class">
+                                <img src={item.url} onClick={this.onImgClick.bind(this,item)} style={{width:300,height:300}}/>
+                            </li>
+                        )
+                    })
+                }
+
+            </Masonry>
         )
     }
 
