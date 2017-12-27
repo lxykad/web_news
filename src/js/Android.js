0ba753fe0faf709a4006813218ad68e6388c7e28
1 <a@1deiMac-4.local>
2017-12-27 11:32:08 +0800

    添加加载更多分页功能

--- src/js/Android.js
+++ src/js/Android.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import TimeUtil from '../utils/TimeUtil'
-import List from 'antd/lib/list';
+import {List, Avatar, Button, Spin} from 'antd';
 import 'antd/lib/list/style/css'
 import 'antd/lib/avatar/style/css'
 
@@ -9,11 +9,14 @@ class Android extends Component {
     constructor() {
         super();
         this.state = {
+            page: 1,
             name: 'default',
-            url: 'http://gank.io/api/data/Android/20/1',
-            list: []
+            url: 'http://gank.io/api/data/Android/20/',
+            list: [],
+            loading: true,
+            loadingMore: false,
+            showLoadingMore: true
         }
-
     }
 
     handItemClick(item) {
@@ -24,20 +27,29 @@ class Android extends Component {
     * 一些启动动作 像网络请求等，放着这个生命周期里操作
     * */
     componentWillMount() {
-        this.loadData()
+        this.loadData(1)
     }
 
     render() {
+        const {loading, loadingMore, showLoadingMore, data} = this.state;
+        const loadMore = showLoadingMore ? (
+            <div style={{textAlign: 'center', marginTop: 12, height: 32, lineHeight: '32px'}}>
+                {loadingMore && <Spin/>}
+                {!loadingMore && <Button onClick={this.onLoadMore.bind(this)}>加载更多</Button>}
+            </div>
+        ) : null;
+
         return (
 
             <List
                 dataSource={this.state.list}
+                loadMore={loadMore}
                 renderItem={item => (
                     <List.Item onClick={this.handItemClick.bind(this, item)}>
 
                         <List.Item.Meta
                             title={item.desc}
-                            description={TimeUtil.formartTime(new Date(item.publishedAt),'yyyy-MM-dd')}
+                            description={TimeUtil.formartTime(new Date(item.publishedAt), 'yyyy-MM-dd')}
                         />
 
                     </List.Item>)}
@@ -45,20 +57,29 @@ class Android extends Component {
         )
     }
 
-    loadData() {
+    loadData(page) {
 
-        fetch(this.state.url)
+        var dataUrl = this.state.url + page
+
+        fetch(dataUrl)
             .then(response => response.json())
             .then(data => {
 
                 if (!data.error) {
                     this.setState({
-                        list: data.results
+                       list: this.state.list.concat(data.results)
                     })
                 }
             })
             .catch(e => console.log("error"))
     }
+
+    onLoadMore() {
+        this.state.page++
+        console.log(this.state.page)
+        this.loadData(this.state.page)
+    }
+
 }
 
 export default Android;
